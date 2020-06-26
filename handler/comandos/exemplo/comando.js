195a062a3f135af1ec39b3d22dcd4605af3087f9
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2020-06-26 02:32:12 -0300

    Update comando.js

--- handler/comandos/exemplo/comando.js
+++ handler/comandos/exemplo/comando.js
@@ -1,9 +1,7 @@
 module.exports.info = {
-    cmd: ['exemplo'/*, 'Insira valores para usar como comando.'*/],
-    roles: [/*'Coloque aqui o nome das roles configurada na config.json', 'perm-bot', 'exemplo1', 'exemplo2'*/],
-    users: [/*'Coloque aqui o nome dos users configurado na config.json', 'Fabio', 'exemplo1', 'exemplo2'*/]
+    cmd: ['exemplo'/*, 'Insira valores para usar como comando.'*/]
 }
 
-module.exports.comando = (client, msg, args) => {
+module.exports.comando = (bot, msg, args) => {
     //Aqui é onde ocorre a magina, pois tudo o que for criado neste bloco será executado ao chamar o comando.
 }
