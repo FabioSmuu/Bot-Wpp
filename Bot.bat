f994cd56240fa0d10e0f338b960d71cb9e5078aa
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2020-06-26 02:27:22 -0300

    Add files via upload

---
+++ Bot.bat
@@ -0,0 +1,5 @@
+@echo off
+title BOT
+cls
+node server.js
+pause
---
+++ server.js
@@ -0,0 +1,4 @@
+global.config = require("./handler/config.json")
+module.exports = {
+	Client: require("./handler/core/client.js")
+}
